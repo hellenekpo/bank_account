c4eabda8e067aa1b85414c217c039cbf9532ac84
Hellen Ekpo <he560@nyu.edu>
2022-05-03 14:42:30 -0400

    Create BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bankaccount;
+
+/**
+ *
+ * @author hellenekpo
+ */
+public class BankAccount {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hekllllo");
+        
+    }
+    
+}
