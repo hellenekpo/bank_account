7f807fff7f4994f8f8cb6b96cad7cc312ece6e06
Hellen Ekpo <he560@nyu.edu>
2022-05-03 15:37:43 -0400

    new changes

--- BankAccount.java
+++ BankAccount.java
@@ -3,6 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package bankaccount;
+import java.awt.*;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.util.Random;
 
 /**
  *
@@ -22,6 +30,15 @@ public class BankAccount {
         //The three concepts we would like to use are
         //graphics, file i/o and thread concurrency.
         System.out.println("hekllllo");
+        JFrame jf = new JFrame("Bank Account!");
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setSize(1200, 800);
+        jf.setResizable(false);
+        JButton [] buttons = new JButton[10];
+        boolean [] toggle = new boolean[10];
+       
+        
+        
         
     }
     
